1152dd2d603300994c55cdd00c93710acc54da9a
shiv sharma <shiv_softengg@yahoo.com>
2014-02-17 17:44:25 +0000

    modified wordpress

--- modules/wordpress/manifests/init.pp
+++ modules/wordpress/manifests/init.pp
@@ -14,27 +14,27 @@ class wordpress {
 
   file {
   '/var/www/vhosts/lin-test.rackstage.co.uk/public_html/wp-config.php':
-     ensure  => present,
-     require => [Exec['wordpress'],File['/var/www/vhosts/lin-test.rackstage.co.uk/public_html']],
-     source  => "puppet:///modules/wordpress/var/wp-config.php",
-     owner   => nobody,
-     group   => nogroup,
-     notify  => Class['apache::service'];
+    ensure  => 'present',
+    require => [Exec['wordpress'],File['/var/www/vhosts/lin-test.rackstage.co.uk/public_html']],
+    source  => "puppet:///modules/wordpress/var/wp-config.php",
+    owner   => nobody,
+    group   => nogroup,
+    notify  => Class['apache::service'];
   '/var/www/vhosts/lin-test.rackstage.co.uk/public_html':
-     ensure => directory,
-     owner => nobody,
-     group => nogroup,
-     require => File['/var/www/vhosts/lin-test.rackstage.co.uk'];
+   ensure => directory,
+   owner => nobody,
+   group => nogroup,
+   require => File['/var/www/vhosts/lin-test.rackstage.co.uk'];
   '/var/www/vhosts/lin-test.rackstage.co.uk':
-     ensure => directory,
-     owner => root,
-     group => root,
-     require => File['/var/www/vhosts'];
+   ensure => directory,
+   owner => root,
+   group => root,
+   require => File['/var/www/vhosts'];
   '/var/www/vhosts':
-     ensure => directory,
-     owner => root,
-     group => root,
-     require => Class['apache::package'];
+   ensure => directory,
+   owner => root,
+   group => root,
+   require => Class['apache::package'];
    
    }
 }
